87ffbfa956e0e18d108748db9b298d1fa5402446
codegeek004 <yashvaishnav1404@gmail.com>
2026-01-14 23:47:14 +0530

    added paths for UserInfoView, DiseaseView, FitnessInfoView, WorkoutPlanView

--- fitness/urls.py
+++ fitness/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
 from fitness.views import (
+    DiseaseView,
     HomeView,
     RegisterView,
     LoginView,
+    UserInfoView,
+    DiseaseView,
+    FitnessInfoView,
+    WorkoutPlanView
 )
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('user_info/', UserInfoView.as_view(), name='user_info'),
+    path('diseases/', DiseaseView.as_view(), name='diseases'),
+    path("fitness_info/", FitnessInfoView.as_view(), name='fitness'),
+    path("workout_plan/", WorkoutPlanView.as_view(), name="workout-plan"),
+
 
 ]
