faba3e26b38d3b16435d5c6747eddfcc058c19a8
codegeek004 <yashvaishnav1404@gmail.com>
2026-01-14 14:37:31 +0530

    added home, login and register urls

--- fitness/urls.py
+++ fitness/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
+from fitness.views import (
+    HomeView,
+    RegisterView,
+    LoginView,
+)
 urlpatterns = [
+    path('', HomeView.as_view(), name="home"),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
 
 ]
